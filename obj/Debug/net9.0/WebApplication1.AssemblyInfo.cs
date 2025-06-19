52b23ff1d2d18b301cc0e29e0c6464f0134193ac
Georgi <georgi@Georgis-MacBook-Air.local>
2025-06-19 14:22:36 +0300

    v18

--- obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91d8ef6f93294e2dce260a3217d80c26600bcf6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
